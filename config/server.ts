dbec9217759658da4013085f3eb2c7408197acbb
mohammadyousefi02 <mohammadyousefi02@gmail.com>
2022-07-10 03:55:09 +0430

    add

--- config/server.ts
+++ config/server.ts
@@ -2,7 +2,7 @@ let server:string;
 
 const dev = process.env.NODE_ENV !== "production";
 
-dev ? server = "http://localhost:3000" : server = ""
+dev ? server = "http://localhost:3000" : server = "https://ticket-nu.vercel.app/"
 
 export { server };
 
